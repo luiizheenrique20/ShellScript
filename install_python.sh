fbaf67ae68dd68ffce52da237434e75ddb0ff76e
Luiz Hossi <luiizheenrique.20@gmail.com>
2018-11-21 16:30:26 -0200

    Adc. do paramentro -o StrictHostKeyChecking=no

--- install_python.sh
+++ install_python.sh
@@ -8,16 +8,16 @@ do
 echo $x
 IP=`echo $x | awk '{print $1}'`
 USER=`echo $x | awk '{print $2}' | awk -F"=" '{print $2}'`
-PEM=`echo $x | awk -F"/" '{print $5}'`
+PEM=`echo $x | awk -F"/" '{print $3}'`
 #exit 1
 
 if [ ${USER} = "ubuntu" ]
 then
-ssh -i /home/luizhossi/pem/${PEM} $USER@$IP "sudo apt install python python-pip -y"
+ssh -i /home/luizhossi/pem/${PEM} -o StrictHostKeyChecking=no $USER@$IP "sudo apt install python python-pip -y"
 
 else
 
-ssh -i /home/luizhossi/pem/${PEM} $USER@$IP "sudo yum install python python-pip -y"
+ssh -i /home/luizhossi/pem/${PEM} -o StrictHostKeyChecking=no $USER@$IP "sudo yum install python python-pip -y"
 fi
 
 done
